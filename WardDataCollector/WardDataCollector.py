33c51d1d363ee47aa67e65e3bd2e0a654cf215fa
Robert Fry <85572108+RobertSDG@users.noreply.github.com>
2021-11-17 17:49:08 +0000

    added basic start data

--- WardDataCollector/WardDataCollector.py
+++ WardDataCollector/WardDataCollector.py
@@ -1,5 +1,13 @@
+# File to collect data from opendata.bristol api in a format that can be uploaded to the open data platform
+
 import sys
-def main():
+import csv as csv
+
+# url for the open Data Platform
+URL = "https://opendata.bristol.gov.uk/api/records/1.0/search/?dataset=quality-of-life-2018-19-ward&refine.indicator=%25+satisfied+with+their+local+area"
+
+
+def main(): 
     pass
 
 if __name__ == "__main__":

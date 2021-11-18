82e483c231b5f876bd41fdf5d8936458580afeac
Robert Fry <85572108+RobertSDG@users.noreply.github.com>
2021-11-18 20:31:11 +0000

    added json parsing

--- WardDataCollector/WardDataCollector.py
+++ WardDataCollector/WardDataCollector.py
@@ -3,14 +3,57 @@
 import sys
 import csv as csv
 import requests
-from urllib.parse import urlparse as urlparse
+from urllib.parse import urlparse, urlencode
+
+# Function accepts a json object from request and parses out the data to a array and returns
+# the array
+def parse_data(Json):
+
+    pass
+
+def buildUrl(dataset, refineIndicator):
+    getVars = {'dataset': dataset, 'rows': 50, 'refine.indicator': refineIndicator}
+    baseUrl = "https://opendata.bristol.gov.uk/api/records/1.0/search/?"
+    uri = baseUrl + urlencode(getVars)
+    print(uri)
+    #requests.get(uri)
+    return uri
+
+# throws a excception on connection failed, this should be handled externally to retry after delay
+def getData(url):
+    r = requests.get(url)
+    print(r.status_code)
+
+    #results should be appended in order Ward, GeoCode, value
+    result = []
+    if(r.status_code == 200):
+        # parse json
+        body = r.json()['records'];
+        # print(body);
+        for x in body:
+            ptr = x['fields']
+            if "ward_code" in ptr:
+                res = (ptr['ward_name'], ptr['ward_code'], ptr['statistic'])
+            else:
+                res = (ptr['ward_name'], "", ptr['statistic'])
+            result += res
+            print(res)
+            #print(x) 
+    else:
+        return []
 
 # url for the open Data Platform
 
 def main():
     URL = "https://opendata.bristol.gov.uk/api/records/1.0/search/?dataset=quality-of-life-2018-19-ward&refine.indicator=%25+satisfied+with+their+local+area"
     unity = urlparse(URL)
-    print (unity)
+
+    dataset = "quality-of-life-2018-19-ward"
+    indicator = "% satisfied with their local area"
+    u = buildUrl(dataset, indicator)
+    r = getData(u)
+    print(r)
+    #print (unity)
     #fetch = requests.get(URL)
     #print(fetch.status_code)
     #print(fetch.json())
@@ -19,8 +62,3 @@ def main():
 if __name__ == "__main__":
     sys.exit(int(main() or 0))
 
-# Function accepts a json object from request and parses out the data to a array and returns
-# the array
-def parse_data(Json):
-
-    pass

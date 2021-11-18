3973878f389b43a4e66b0b23b2450977a7853a05
Robert Fry <85572108+RobertSDG@users.noreply.github.com>
2021-11-18 14:26:04 +0000

    added secondary function

--- WardDataCollector/WardDataCollector.py
+++ WardDataCollector/WardDataCollector.py
@@ -5,11 +5,20 @@ import csv as csv
 import requests
 
 # url for the open Data Platform
-URL = "https://opendata.bristol.gov.uk/api/records/1.0/search/?dataset=quality-of-life-2018-19-ward&refine.indicator=%25+satisfied+with+their+local+area"
 
-
-def main(): 
-    pass
+def main():
+    URL = "https://opendata.bristol.gov.uk/api/records/1.0/search/?dataset=quality-of-life-2018-19-ward&refine.indicator=%25+satisfied+with+their+local+area"
+    
+    fetch = requests.get(URL)
+    print(fetch.status_code)
+    print(fetch.json())
+    return
 
 if __name__ == "__main__":
     sys.exit(int(main() or 0))
+
+# Function accepts a json object from request and parses out the data to a array and returns
+# the array
+def parse_data(Json):
+
+    pass

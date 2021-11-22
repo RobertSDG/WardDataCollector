a3d4329a21f45986b690e0a34e3eff790ff06971
Robert Fry <85572108+RobertSDG@users.noreply.github.com>
2021-11-22 15:42:44 +0000

    updated code formaatting

--- WardDataCollector/WardDataCollector.py
+++ WardDataCollector/WardDataCollector.py
@@ -1,80 +1,80 @@
-# File to collect data from opendata.bristol api in a format that can be uploaded to the open data platform
+# File to collect data from opendata.bristol api in a format that can be uploaded
+#  to the open data platform
 
 import sys
 import csv as csv
 import requests
-from urllib.parse import urlparse, urlencode
+from urllib.parse import urlencode
 
 # Function accepts a json object from request and parses out the data to a array and returns
 # the array
-def parse_data(Json):
+def parse_data(json):
 
     pass
 
-def buildUrl(dataset, refineIndicator):
-    getVars = {'dataset': dataset, 'rows': 50, 'refine.indicator': refineIndicator}
-    baseUrl = "https://opendata.bristol.gov.uk/api/records/1.0/search/?"
-    uri = baseUrl + urlencode(getVars)
+# Function to build the URL to query the Open Data API
+def build_url(dataset, refine_indicator):
+    get_vars = {'dataset': dataset, 'rows': 50, 'refine.indicator': refine_indicator}
+    base_url = "https://opendata.bristol.gov.uk/api/records/1.0/search/?"
+    uri = base_url + urlencode(get_vars)
     print(uri)
     #requests.get(uri)
     return uri
 
 # throws a excception on connection failed, this should be handled externally to retry after delay
-def getData(url):
-    r = requests.get(url)
-    print(r.status_code)
+def get_data(url):
+    response = requests.get(url)
+    print(response.status_code)
 
     #results should be appended in order Ward, GeoCode, value
     result = []
-    if(r.status_code == 200):
+    if response.status_code == 200:
         # parse json
-        body = r.json()['records'];
+        body = response.json()['records']
         # print(body);
-        for x in body:
-            ptr = x['fields']
+        for row in body:
+            ptr = row['fields']
             if "ward_code" in ptr:
                 res = (ptr['ward_name'], ptr['ward_code'], ptr['statistic'])
             else:
                 res = (ptr['ward_name'], "", ptr['statistic'])
             result.append(res)
             # print(res)
-            
-            #print(x) 
+
+            #print(x)
     return result
 
-# url for the open Data Platform
-
+# Main entry for script
 def main():
-    URL = "https://opendata.bristol.gov.uk/api/records/1.0/search/?dataset=quality-of-life-2018-19-ward&refine.indicator=%25+satisfied+with+their+local+area"
-    unity = urlparse(URL)
-    datasetYear = [
+    # URL = "https://opendata.bristol.gov.uk/api/records/1.0/search/?
+    # dataset=quality-of-life-2018-19-ward&
+    # refine.indicator=%25+satisfied+with+their+local+area"
+    dataset_year = [
         "2017-18",
         "2018-19",
         "2019-20",
         "2020-21"
         ]
-    datasetTemplate = "quality-of-life-2018-19-ward"
     dataset = "quality-of-life-2018-19-ward"
     indicator = "% satisfied with their local area"
-    u = buildUrl(dataset, indicator)
-    r = getData(u)
-    dict = {}
+    url = build_url(dataset, indicator)
+    response = get_data(url)
+    datasets = {}
 
-    for x in datasetYear:
-        template = f'quality-of-life-{x}-ward'
+    for row in dataset_year:
+        template = f'quality-of-life-{row}-ward'
         print(template)
-        u = buildUrl(template, indicator)
-        r = getData(u)
-        dict[x]=r
+        url = build_url(template, indicator)
+        response = get_data(url)
+        datasets[row]=response
 
-    print(dict)
+    print(datasets)
 
     #print (unity)
     #fetch = requests.get(URL)
     #print(fetch.status_code)
     #print(fetch.json())
-    return
+    return 0
 
 if __name__ == "__main__":
     sys.exit(int(main() or 0))
-

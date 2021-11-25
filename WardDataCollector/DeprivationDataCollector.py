801667ac55c874b33c6d667dccea109e622f66b9
Robert Fry <85572108+RobertSDG@users.noreply.github.com>
2021-11-25 15:16:34 +0000

    add dataset url builder

--- WardDataCollector/DeprivationDataCollector.py
+++ WardDataCollector/DeprivationDataCollector.py
@@ -1,2 +1,17 @@
 class DeprivationDataCollector(object):
     """Class to access time series of deprivation data from bristol open data api"""
+
+    __datasets = {
+        "2017-18":"qol-deprivation-dataset-2017_18-v2",
+        "2018-19":"quality-of-life-2018-19-deprivation",
+        "2019-20":"quality-of-life-2019-20-deprivation",
+        "2020-21":"quality-of-life-2020-21-deprivation"
+        }
+
+    def build_url(dataset, refine_indicator):
+        get_vars = {"dataset": dataset, "rows": 50, "refine.indicator": refine_indicator}
+        base_url = "https://opendata.bristol.gov.uk/api/records/1.0/search/?"
+        uri = base_url + urlencode(get_vars)
+        print(uri)
+        # requests.get(uri)
+        return uri

dff6d16f8d861d89d0b55f9e17d9b7f469b6e643
RobertSDG <RobertSDG@users.noreply.github.com>
2021-11-25 14:50:57 +0000

    :art: Format Python code with psf/black

--- WardDataCollector/WardDataCollector.py
+++ WardDataCollector/WardDataCollector.py
@@ -11,48 +11,47 @@ def parse_data(Json):
 
     pass
 
+
 def buildUrl(dataset, refineIndicator):
-    getVars = {'dataset': dataset, 'rows': 50, 'refine.indicator': refineIndicator}
+    getVars = {"dataset": dataset, "rows": 50, "refine.indicator": refineIndicator}
     baseUrl = "https://opendata.bristol.gov.uk/api/records/1.0/search/?"
     uri = baseUrl + urlencode(getVars)
     print(uri)
-    #requests.get(uri)
+    # requests.get(uri)
     return uri
+
 
 # throws a excception on connection failed, this should be handled externally to retry after delay
 def getData(url):
     r = requests.get(url)
     print(r.status_code)
 
-    #results should be appended in order Ward, GeoCode, value
+    # results should be appended in order Ward, GeoCode, value
     result = []
-    if(r.status_code == 200):
+    if r.status_code == 200:
         # parse json
-        body = r.json()['records'];
+        body = r.json()["records"]
         # print(body);
         for x in body:
-            ptr = x['fields']
+            ptr = x["fields"]
             if "ward_code" in ptr:
-                res = (ptr['ward_name'], ptr['ward_code'], ptr['statistic'])
+                res = (ptr["ward_name"], ptr["ward_code"], ptr["statistic"])
             else:
-                res = (ptr['ward_name'], "", ptr['statistic'])
+                res = (ptr["ward_name"], "", ptr["statistic"])
             result.append(res)
             # print(res)
-            
-            #print(x) 
+
+            # print(x)
     return result
 
+
 # url for the open Data Platform
+
 
 def main():
     URL = "https://opendata.bristol.gov.uk/api/records/1.0/search/?dataset=quality-of-life-2018-19-ward&refine.indicator=%25+satisfied+with+their+local+area"
     unity = urlparse(URL)
-    datasetYear = [
-        "2017-18",
-        "2018-19",
-        "2019-20",
-        "2020-21"
-        ]
+    datasetYear = ["2017-18", "2018-19", "2019-20", "2020-21"]
     datasetTemplate = "quality-of-life-2018-19-ward"
     dataset = "quality-of-life-2018-19-ward"
     indicator = "% satisfied with their local area"
@@ -61,20 +60,20 @@ def main():
     dict = {}
 
     for x in datasetYear:
-        template = f'quality-of-life-{x}-ward'
+        template = f"quality-of-life-{x}-ward"
         print(template)
         u = buildUrl(template, indicator)
         r = getData(u)
-        dict[x]=r
+        dict[x] = r
 
     print(dict)
 
-    #print (unity)
-    #fetch = requests.get(URL)
-    #print(fetch.status_code)
-    #print(fetch.json())
+    # print (unity)
+    # fetch = requests.get(URL)
+    # print(fetch.status_code)
+    # print(fetch.json())
     return
+
 
 if __name__ == "__main__":
     sys.exit(int(main() or 0))
-

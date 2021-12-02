e5e1bd0d534d9c5e57ef4fd9c77d9de88788d9a7
RobertSDG <RobertSDG@users.noreply.github.com>
2021-12-02 14:02:20 +0000

    :art: Format Python code with psf/black

--- WardDataCollector/DeprivationDataCollector.py
+++ WardDataCollector/DeprivationDataCollector.py
@@ -4,26 +4,31 @@ from urllib.parse import urlencode
 
 import requests
 
+
 class DeprivationDataCollector(object):
     """Class to access time series of deprivation data from bristol open data api"""
 
     __datasets = {
-        "2017-18":"qol-deprivation-dataset-2017_18-v2",
-        "2018-19":"quality-of-life-2018-19-deprivation",
-        "2019-20":"quality-of-life-2019-20-deprivation",
-        "2020-21":"quality-of-life-2020-21-deprivation"
-        }
+        "2017-18": "qol-deprivation-dataset-2017_18-v2",
+        "2018-19": "quality-of-life-2018-19-deprivation",
+        "2019-20": "quality-of-life-2019-20-deprivation",
+        "2020-21": "quality-of-life-2020-21-deprivation",
+    }
 
-    def build_url(self, dataset:str, refine_indicator:str):
-        """ builds the url to access a dataset """
-        get_vars = {"dataset": dataset, "rows": 50, "refine.indicator": refine_indicator}
+    def build_url(self, dataset: str, refine_indicator: str):
+        """builds the url to access a dataset"""
+        get_vars = {
+            "dataset": dataset,
+            "rows": 50,
+            "refine.indicator": refine_indicator,
+        }
         base_url = "https://opendata.bristol.gov.uk/api/records/1.0/search/?"
         uri = base_url + urlencode(get_vars)
         print(uri)
-        # requests.get(uri) 
+        # requests.get(uri)
         return uri
 
-    def get_data(self, url:str):
+    def get_data(self, url: str):
         response = requests.get(url)
         print(response.status_code)
 
@@ -32,22 +37,22 @@ class DeprivationDataCollector(object):
         if response.status_code == 200:
             # parse json
             body = response.json()["records"]
-        # print(body);
+            # print(body);
             for row in body:
                 ptr = row["fields"]
                 # 'deprivation_decile': ['Least Deprived 10%','.Most Deprived 10%']
                 # if basic field exists
-                if 'deprivation_decile' in ptr:
-                    deprivation_decile = ptr['deprivation_decile'].lower()
+                if "deprivation_decile" in ptr:
+                    deprivation_decile = ptr["deprivation_decile"].lower()
                     # deprivation_decile = deprivation_decile.lower()
                     print(deprivation_decile + " is the label")
-                    if deprivation_decile.find('least deprived 10%') != -1:
+                    if deprivation_decile.find("least deprived 10%") != -1:
                         # check for lower 10
-                        result.append(['Least Deprived 10%',  ptr["statistic"]])
+                        result.append(["Least Deprived 10%", ptr["statistic"]])
 
-                    elif deprivation_decile.find('most deprived 10%') != -1:
+                    elif deprivation_decile.find("most deprived 10%") != -1:
                         # check for upper 10
-                        result.append(['Most Deprived 10%',  ptr["statistic"]])
+                        result.append(["Most Deprived 10%", ptr["statistic"]])
 
                 # print(x)
         return result
@@ -62,13 +67,17 @@ class DeprivationDataCollector(object):
             result_set[key] = response
         return result_set
 
-    
 
 def main():
     """entry point for quick dirty testing"""
     deprivation_data_collector = DeprivationDataCollector()
-    print (deprivation_data_collector.dataset_builder("% households which have experienced moderate to severe food insecurity"))
+    print(
+        deprivation_data_collector.dataset_builder(
+            "% households which have experienced moderate to severe food insecurity"
+        )
+    )
     pass
+
 
 if __name__ == "__main__":
     sys.exit(int(main() or 0))

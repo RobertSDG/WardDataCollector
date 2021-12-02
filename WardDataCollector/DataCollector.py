92d694477eebb74040d03d2a98ca88b66dbd08d0
Robert Fry <85572108+RobertSDG@users.noreply.github.com>
2021-12-02 15:16:32 +0000

    Create DataCollector.py

---
+++ WardDataCollector/DataCollector.py
@@ -0,0 +1,14 @@
+import sys
+class DataCollector(object):
+    """description of class"""
+    
+
+
+
+
+def main():
+    """entry point for quickly checking function"""
+    pass
+
+if __name__ == "__main__":
+    sys.exit(int(main() or 0))

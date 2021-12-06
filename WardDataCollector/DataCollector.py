a7619395c147c4c08789c760aa218f19b310b8d5
Robert Fry <85572108+RobertSDG@users.noreply.github.com>
2021-12-06 09:46:20 +0000

    updates to enable pulling different datasets
    updates to pull data

--- WardDataCollector/DataCollector.py
+++ WardDataCollector/DataCollector.py
@@ -9,13 +9,24 @@ class DataCollector(object):
     def __init__(self,indicator):
         self.indicator = indicator
 
+    def get_data (self)->dict:
+        results = {}
+        results['Ward'] = WardDataCollector.dataset_builder(self.indicator)
+        deprivation = DeprivationDataCollector(self.indicator)
+        results['Deprivation'] = deprivation.dataset_builder(self.indicator)
+
+        return results
 
 
 
 
 def main():
     """entry point for quickly checking function"""
-    pass
+    indicator = "% households which have experienced severe food insecurity"
+    d = DataCollector(indicator)
+    i = d.get_data()
+    print(i)
+    return 0
 
 if __name__ == "__main__":
     sys.exit(int(main() or 0))
--- WardDataCollector/WardDataCollector.py
+++ WardDataCollector/WardDataCollector.py
@@ -92,6 +92,7 @@ def data_to_csv_multiple(collection: dict, file_name: str) -> None:
 
 
 def dataset_builder(indicator: str) -> dict:
+    """returns dataset for the indicator passed"""
     dataset_year = ["2017-18", "2018-19", "2019-20", "2020-21"]
     datasets = {}
 
